423bf1a140fc81e63da96f15a7e6f061c3ec5cd7
Zlatin Zlatev <103603053+Zlatev88@users.noreply.github.com>
2022-05-21 16:02:44 +0300

    Add files via upload

---
+++ Stacks and Queues/Lab/DecimalToBinaryConverter.java
@@ -0,0 +1,28 @@
+package StackAndQueues.Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class DecimalToBinaryConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int decimal = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        if (decimal==0) {
+            System.out.println(0);
+        }
+        else {
+
+            while (decimal!=0) {
+
+                stack.push(decimal%2);
+                decimal/=2;
+            }
+            for (int element : stack) {
+                System.out.print(stack.pop());
+            }
+        }
+
+    }
+}
