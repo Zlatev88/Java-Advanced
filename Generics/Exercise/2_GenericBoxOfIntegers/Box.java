2dfecc501ec85051483d693f86a430e13dd83865
Zlatin Zlatev <103603053+Zlatev88@users.noreply.github.com>
2022-11-01 23:15:51 +0200

    Add files via upload

---
+++ Generics/Exercise/2_GenericBoxOfIntegers/Box.java
@@ -0,0 +1,26 @@
+package Generics.Exercise._2_GenericBoxOfIntegers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box <Т>{
+    private List<Т> values;
+
+    public Box() {
+        this.values = new ArrayList<>();
+    }
+
+    public void add(Т value) {
+        values.add(value);
+    }
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        for (Т value : values) {
+            sb.append(String.format("%s: %s%n", value.getClass().getName(), value));
+        }
+
+        return sb.toString();
+    }
+}
+
